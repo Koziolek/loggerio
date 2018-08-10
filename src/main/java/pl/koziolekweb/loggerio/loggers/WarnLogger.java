577e9b4ae7046121211c42b1cb92c87ca7601e20
Bartek Kuczynski <bartlomiej.kuczynski@incat.com.pl>
2018-08-10 12:37:53 +0200

    Add snapshot repository

--- src/main/java/pl/koziolekweb/loggerio/loggers/WarnLogger.java
+++
@@ -1,9 +0,0 @@
-package pl.koziolekweb.loggerio.loggers;
-
-import java.util.function.Consumer;
-
-public final class WarnLogger extends AbstractLogger{
-	public WarnLogger(Consumer<String> target) {
-		super(target);
-	}
-}
